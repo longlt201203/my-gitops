2bf48fb4fced4a8537deca5156138f6a2fcba076
Le thanh Long <longlt201203@nexondv.com>
2024-07-05 17:26:14 +0700

    update

--- generate.js
+++ generate.js
@@ -2,30 +2,52 @@ console.log("Calling generate function with parameters:", process.argv);
 
 const project = process.argv[2];
 const runId = process.argv[3];
+const registry = process.argv[4] || "longthanhle1122";
 
 const fs = require("fs");
 const path = require("path");
 
-const deploymentTemplatePath = path.resolve(
-  __dirname,
-  "templates",
-  `${project}.deployment.yaml`
-);
+const projectFolder = path.resolve(__dirname, project);
+
+console.log("Finding project...");
+if (!fs.existsSync(projectFolder)) {
+  console.log("Project not found! Creating new...");
+  fs.mkdirSync(projectFolder);
+  const serviceContent = fs
+    .readFileSync(path.resolve(__dirname, "templates", "serivce.yaml"))
+    .toString()
+    .replace("{{ project }}", project);
+  fs.writeFileSync(
+    path.resolve(__dirname, project, "service.yaml"),
+    serviceContent
+  );
+  const deploymentTemplateContent = fs
+    .readFileSync(path.resolve(__dirname, "templates", "deployment.yaml"))
+    .toString()
+    .replace("{{ project }}", project)
+    .replace("{{ registry }}", registry);
+  fs.writeFileSync(
+    path.resolve(__dirname, "templates", `${project}.deployment.yaml`),
+    deploymentTemplateContent
+  );
+  console.log("Project created!");
+} else {
+  console.log("Project found!");
+}
 
 console.log("Reading and replacing the content of deloyment template...");
 const deploymentContent = fs
-  .readFileSync(deploymentTemplatePath)
+  .readFileSync(
+    path.resolve(__dirname, "templates", `${project}.deployment.yaml`)
+  )
   .toString()
   .replace("{{ run_id }}", runId);
 
-const deploymentFileOutput = path.resolve(
-  __dirname,
-  project,
-  "deployment.yaml"
-);
-
 console.log("Writing output to deployment.yaml file...");
-fs.writeFileSync(deploymentFileOutput, deploymentContent);
+fs.writeFileSync(
+  path.resolve(__dirname, project, "deployment.yaml"),
+  deploymentContent
+);
 console.log("Finish writing output.");
 
 const dir = fs.readdirSync(path.resolve(__dirname, project));
